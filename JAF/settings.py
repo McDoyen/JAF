91d379149032943596c6f60b09960bdc36ad2d84
Paul <mrpoloh@yahoo.co.uk>
2022-06-18 23:10:47 +0300

    Add education app

--- JAF/settings.py
+++ JAF/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'education.apps.EducationConfig',
     'introduction.apps.IntroductionConfig',
     'django.contrib.admin',
     'django.contrib.auth',
