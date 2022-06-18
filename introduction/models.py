83dde50b419203a9992816dac928bc86a4f0e4e3
Paul <mrpoloh@yahoo.co.uk>
2022-06-18 14:35:05 +0300

    Register the personal information model

--- introduction/models.py
+++ introduction/models.py
@@ -3,4 +3,16 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    INPUT_TYPES = (
+        ('date', 'date'), ('email', 'email'), ('file', 'file'), ('image',
+                                                                 'image'), ('number', 'number'), ('tel', 'tel'), ('text', 'text')
+    )
+    question_text = models.CharField(max_length=200)
+    question_index = models.IntegerField(default=0)
+    input_type = models.CharField(
+        default='text', max_length=6, choices=INPUT_TYPES)
+
+    def __str__(self):
+        return self.question_text
