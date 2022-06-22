7db79bd41a085c825d5a4ea102fde6a8213940fb
Paul <mrpoloh@yahoo.co.uk>
2022-06-23 02:15:48 +0300

    Code refactor

--- forms/models.py
+++ forms/models.py
@@ -3,12 +3,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+INPUT_TYPES = (('text', 'text'), ('date', 'date'))
+
 
 class PersonalInformation(models.Model):
-    INPUT_TYPES = (
-        ('date', 'date'), ('email', 'email'), ('file', 'file'), ('image',
-                                                                 'image'), ('number', 'number'), ('tel', 'tel'), ('text', 'text')
-    )
+    INPUT_TYPES = (('text', 'text'), ('date', 'date'), ('email', 'email'),
+                   ('file', 'file'), ('image', 'image'), ('number', 'number'), ('tel', 'tel'))
     question_text = models.CharField(max_length=200)
     question_index = models.IntegerField(default=0)
     input_type = models.CharField(
@@ -19,8 +19,6 @@ class PersonalInformation(models.Model):
 
 
 class AcademicQualification(models.Model):
-    INPUT_TYPES = (('date', 'date'), ('text', 'text'))
-
     title = models.CharField(max_length=200)
     title_index = models.IntegerField(default=0)
     input_type = models.CharField(
@@ -31,8 +29,6 @@ class AcademicQualification(models.Model):
 
 
 class WorkingExperience(models.Model):
-    INPUT_TYPES = (('text', 'text'), ('date', 'date'))
-
     title = models.CharField(max_length=200)
     title_index = models.IntegerField(default=0)
     input_type = models.CharField(
@@ -43,8 +39,7 @@ class WorkingExperience(models.Model):
 
 
 class Reference(models.Model):
-    INPUT_TYPES = (('tel', 'tel'),
-                   ('text', 'text'), ('email', 'email'))
+    INPUT_TYPES = (('text', 'text'), ('tel', 'tel'), ('email', 'email'))
 
     title = models.CharField(max_length=200)
     title_index = models.IntegerField(default=0)
--- forms/views.py
+++ forms/views.py
@@ -9,38 +9,32 @@ from .models import WorkingExperience
 from .models import Reference
 
 
+def getQuerySet(model, orderMethod):
+    return model.objects.values().order_by(orderMethod)
+
+
+def inputTypeQuerySet(model):
+    return model.objects.values_list('input_type', flat=True)
+
+
+def context(model):
+    return {'latest_colomn_list': list(
+        getQuerySet(model, 'title_index')), "input_type_query_set": list(inputTypeQuerySet(model))}
+
+
 def introduction(request):
-    latest_question_list = PersonalInformation.objects.order_by('question_index')[
-        :50]
-    context = {'latest_question_list': latest_question_list}
+    context = {'latest_question_list': getQuerySet(
+        PersonalInformation, 'question_index')}
     return render(request, 'forms/index.html', context)
 
 
 def education(request):
-    latest_colomn_list = AcademicQualification.objects.values(
-        'title', 'input_type').order_by('title_index')
-    input_type_query_set = AcademicQualification.objects.values_list(
-        'input_type', flat=True)
-    context = {'latest_colomn_list': list(
-        latest_colomn_list), "input_type_query_set": list(input_type_query_set)}
-    return render(request, 'forms/education.html', context)
+    return render(request, 'forms/education.html', context(AcademicQualification))
 
 
 def work(request):
-    latest_colomn_list = WorkingExperience.objects.values(
-        'title', 'input_type').order_by('title_index')
-    input_type_query_set = WorkingExperience.objects.values_list(
-        'input_type', flat=True)
-    context = {'latest_colomn_list': list(
-        latest_colomn_list), "input_type_query_set": list(input_type_query_set)}
-    return render(request, 'forms/work.html', context)
+    return render(request, 'forms/work.html', context(WorkingExperience))
 
 
 def reference(request):
-    latest_colomn_list = Reference.objects.values(
-        'title', 'input_type').order_by('title_index')
-    input_type_query_set = Reference.objects.values_list(
-        'input_type', flat=True)
-    context = {'latest_colomn_list': list(
-        latest_colomn_list), "input_type_query_set": list(input_type_query_set)}
-    return render(request, 'forms/reference.html', context)
+    return render(request, 'forms/reference.html', context(Reference))
